0360776553120b5ef77c497d4cff45fd7f54b390
unknown <theodoraamodu1@gmail.com>
2024-05-16 09:26:56 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+/*for the moving pics*/
+let slideIndex = 0;
+
+showSlides();
+var tempo = setInterval(showSlides, 2000); // Change image every 2 seconds
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+
+}
+function changeImage(texto)
+{
+  slideIndex=texto-1;
+  showSlides();
+clearInterval(tempo);
+
+}
+
+
+
+const popup = document.querySelector('.popup');
+  const popupImage = document.getElementById('popupImage');
+  const popupDescription = document.getElementById('popupDescription');
+  const popupClose = document.querySelector('.popup-close');
+
+  function openPopup(element) {
+    const src = element.src;
+    const alt = element.alt;
+    const description = element.dataset.description;
+
+    popupImage.src = src;
+    popupImage.alt = alt;
+    popupDescription.textContent = description;
+
+    popup.classList.add('active');
+  }
+
+  function closePopup() {
+    popup.classList.remove('active');
+  }
+
+  popupClose.addEventListener('click', closePopup);
+
+  
